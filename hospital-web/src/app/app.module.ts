756f1f4602e210876d41098221ce4dff2165ba65
psrinivas-boomplus <92921885+psrinivas-boomplus@users.noreply.github.com>
2022-05-04 14:21:36 +0530

    Add Final ui

--- hospital-web/src/app/app.module.ts
+++ hospital-web/src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './sherd-components/header/header.component';
+import { FooterComponent } from './sherd-components/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { AboutComponent } from './about/about.component';
+import { DoctorsComponent } from './doctors/doctors.component';
+import { BookComponent } from './book/book.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { BlogsComponent } from './blogs/blogs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ServicesComponent,
+    AboutComponent,
+    DoctorsComponent,
+    BookComponent,
+    ReviewsComponent,
+    BlogsComponent
   ],
   imports: [
     BrowserModule
